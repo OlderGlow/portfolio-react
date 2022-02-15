8fb9fc1e533ada7570a6338e72c75f7a10e4de87
Julien Picquet <julienpicquet1@gmail.com>
2022-02-15 21:32:03 +0100

    Add Project page WIP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import HeadPage from "./components/head-page/head-page";
 import AboutMe from "./components/about-me/about-me";
 import Technologies from "./components/technologies/technologies";
+import Projects from "./components/projects/projects";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <HeadPage/>
             <AboutMe/>
             <Technologies/>
+            <Projects/>
         </div>
     );
 }

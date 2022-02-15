b050b9ea5bc0968745f48b9636dba7ed1234fa35
Julien Picquet <julienpicquet1@gmail.com>
2022-02-15 22:31:21 +0100

    add projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,4 +1,6 @@
 import './projects.css';
+import AtelierCreatif from '../../img/projects/atelier-creatif.png';
+import Drunk from '../../img/projects/drunk.png';
 function Projects() {
     return (
         <div className='projects-container'>
@@ -13,8 +15,30 @@ function Projects() {
                     <h1>Projets</h1>
                 </div>
             </div>
-            <div className='projects-left'></div>
-            <div className='projects-right'></div>
+            <div className='projects-wrapper'>
+                <div className='projects-left'>
+                    <div className='projects-left-wrapper'>
+                        <h2>L'Atelier Créatif</h2>
+                        <a href="https://latelier-creatif.vercel.app" target="_blank" rel='noreferrer'>
+                            <img
+                                className='projects-left-img'
+                                src={AtelierCreatif}
+                                alt="L'Atelier Créatif"/>
+                        </a>
+                    </div>
+                </div>
+                <div className='projects-right'>
+                    <div className='projects-left-wrapper'>
+                        <h2>Drunk</h2>
+                        <a href="https://drunk-game-react.vercel.app" target="_blank" rel='noreferrer'>
+                            <img
+                                className='projects-left-img'
+                                src={Drunk}
+                                alt="Drunk"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
